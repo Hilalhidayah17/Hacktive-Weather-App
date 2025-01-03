33235801f5088534d7a87efd00b6539fdff44779
hilal <hilalhidayah589456@gmail.com>
2025-01-03 14:48:56 +0700

    feature: menggunakan API geocoding untuk menangkap kota yang di cari

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 async function fetchData() {
   try {
+    const city = document.getElementById("search-location").value;
+
+    const responseLocation = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+    );
+    const dataLocation = await responseLocation.json();
+
     const response = await fetch(
       "https://api.open-meteo.com/v1/forecast?latitude=3.5833&longitude=98.6667&current=temperature_2m&daily=temperature_2m_max&timezone=Asia%2FBangkok&forecast_days=7"
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    console.log(dataLocation.results);
   } catch (error) {
     console.error(error);
   }

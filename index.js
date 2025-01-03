6b87066085e821999dc8de54cb3056797aa5e356
hilal <hilalhidayah589456@gmail.com>
2025-01-03 15:45:49 +0700

    feature: mendapatkan current weather dan menampilkannya di web

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 async function fetchData() {
   try {
     const city = document.getElementById("search-location").value;
-
     const responseLocation = await fetch(
       `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
     );
     const dataLocation = await responseLocation.json();
 
     const response = await fetch(
-      "https://api.open-meteo.com/v1/forecast?latitude=3.5833&longitude=98.6667&current=temperature_2m&daily=temperature_2m_max&timezone=Asia%2FBangkok&forecast_days=7"
+      `https://api.open-meteo.com/v1/forecast?latitude=${dataLocation.results[0].latitude}&longitude=${dataLocation.results[0].longitude}&current=temperature_2m&daily=temperature_2m_max&timezone=Asia%2FBangkok&forecast_days=7`
     );
     const data = await response.json();
-    // console.log(data);
-    console.log(dataLocation.results);
+
+    document.getElementById(
+      "current-temperature"
+    ).innerText = `${data.current.temperature_2m}°C`;
+    console.log(data.current.temperature_2m);
   } catch (error) {
     console.error(error);
   }

9470fe5de17540d8ee7ac8fa59137894e9ce8e33
hilal <hilalhidayah589456@gmail.com>
2025-01-05 18:56:00 +0700

    feature : menampilkan forecast cuaca selama 7 hari kedepan

--- index.js
+++ index.js
@@ -10,48 +10,29 @@ async function fetchData() {
       `https://api.open-meteo.com/v1/forecast?latitude=${dataLocation.results[0].latitude}&longitude=${dataLocation.results[0].longitude}&current=temperature_2m&daily=temperature_2m_max&timezone=Asia%2FBangkok&forecast_days=7`
     );
     const data = await response.json();
+    console.log(dataLocation.results[0].name);
 
+    // current temperature
+    document.getElementById(
+      "city"
+    ).innerText = `${dataLocation.results[0].name}`;
     document.getElementById(
       "current-temperature"
     ).innerText = `${data.current.temperature_2m}°C`;
 
     // Daily forecast
-    const forecast = document.getElementsByClassName("forecast");
-    data.daily.forEach((e) => {
-      forecast += `<div class="day">
-          <p>Mon</p>
+    let forecast = document.getElementById("forecast");
+    data.daily.time.forEach((e, index) => {
+      const day = e;
+      const maxTemp = data.daily.temperature_2m_max[index];
+      forecast.innerHTML += `<div class="day">
+          <p>${day}</p>
           <img src="" alt="weather" />
-          <p>33&deg; / 27&deg;</p>
+          <p>${maxTemp};</p>
         </div>
-        <div class="day">
-          <p>Tue</p>
-          <img src="" alt="weather" />
-          <p>32&deg; / 27&deg;</p>
-        </div>
-        <div class="day">
-          <p>Wed</p>
-          <img src="" alt="weather" />
-          <p>32&deg; / 27&deg;</p>
-        </div>
-        <div class="day">
-          <p>Thu</p>
-          <img src="" alt="weather" />
-          <p>32&deg; / 27&deg;</p>
-        </div>
-        <div class="day">
-          <p>Fri</p>
-          <img src="" alt="weather" />
-          <p>33&deg; / 27&deg;</p>
-        </div>
-        <div class="day">
-          <p>Sat</p>
-          <img src="" alt="weather" />
-          <p>32&deg; / 27&deg;</p>
-        </div>`;
+      `;
     });
   } catch (error) {
     console.error(error);
   }
 }
-
-// fetchData();
